d5b23d1cae8df6659f745984b448a197fcc76a45
joseph lopez <jalepeno77@gmail.com>
2023-11-08 04:59:18 -0500

    Assignment09 correction

--- assignment09/chain_gang.sql
+++ assignment09/chain_gang.sql
@@ -16,5 +16,4 @@ CREATE TABLE `bicycles` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
-INSERT INTO `bicycles` VALUES (1,'Trek','Emonda',2017,'Hybrid','Unisex','black',1495.00,1.50000,5,''),(2,'Cannondale','Synapse',2016,'Road','Unisex','matte black',1999.00,1.00000,5,'');
-
+INSERT INTO `bicycles` VALUES (1,'Trek','Emonda',2017,'Hybrid','Unisex','black',1495.00,1.50000,5,''),(2,'Cannondale','Synapse',2016,'Road','Unisex','matte black',1999.00,1.00000,5,''),(3,'Schwinn','Cutter',2016,'City','Unisex','white',450.00,18.00000,4,''),(4,'Mongoose','Switchback Sport',2015,'Mountain','Mens','blue',399.00,24.00000,2,''),(5,'Diamondback','Overdrive',2016,'Mountain','Unisex','dark green',565.00,23.70000,3,''),(6,'Schwinn','21-Speed Suburban CS',2015,'Hybrid','Womens','burgundy',299.00,20.00000,3,''),(7,'Schwinn','Sanctuary 7-Speed',2016,'Cruiser','Womens','purple',190.00,19.50000,3,'');
