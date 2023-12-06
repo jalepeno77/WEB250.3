c0c507f32dd8f15f1b87a8bbe8ae0b560ebd7780
jalepeno77 <josephrlopez@students.abtech.edu>
2023-12-06 05:36:23 -0500

    money format deprecation

--- assignment11/chain_gang/public/bicycles.php
+++ assignment11/chain_gang/public/bicycles.php
@@ -38,7 +38,7 @@ $bikes = Bicycle::find_all();
         <td><?php echo h($bike->category); ?></td>
         <td><?php echo h($bike->gender); ?></td>
         <td><?php echo h($bike->color); ?></td>
-        <td><?php echo h(money_format('$%i', $bike->price)); ?></td>
+        <td><?php echo h(number_format($bike->price)); ?></td>
         <td><a href="detail.php?id=<?php echo $bike->id; ?>">View</a></td>
       </tr>
       <?php } ?>
